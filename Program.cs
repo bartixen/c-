f4af54216ebae584f5c58d3fa2b62c40d0251113
Bartixen <macs234hine@gmail.com>
2022-11-25 12:50:27 +0100

    1.0

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metoda metoda = new Metoda();
+            metoda.Sortowanie();
+        }
+       
+           
+    }
+    public class Metoda
+    {
+        int[] t = new int[10];
+
+        private int Minim(int j)
+        {
+            int min = t[j], index = j;
+            for (int i = 1; i < t.Length; i++)
+            {
+                if (t[i] < min)
+                {
+                    min = t[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public void Sortowanie()
+        {
+            int x = Minim(0);
+            Console.WriteLine("najmniejsza wartosx to " + t[x] + " na index " + x);
+        }
+
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie
+{
+
+    public class Tablice
+    {
+        public int[] t = new int[5];
+        public string napis = "";
+
+        public void wczytywanie()
+        {
+            for (int i = 0; i < t.Length; i++) {
+                Console.Write("Podaj liczbe " + (i + 1) + " ");
+                t[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+        }
+        public void wartosci()
+        {
+
+            foreach (int w in t)
+            {
+                napis += w + "; ";
+            }
+        }
+
+        public int mat(char z)
+        {
+            int wynik = t[0];
+            
+            if(z == '+')
+            {
+                foreach (int w in t)
+                {
+                    wynik += w; 
+                }
+            }else if(z == '*')
+            {
+                
+                foreach(int w in t)
+                {
+                    wynik *= w;
+                }
+            }
+
+           return wynik;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablice tablice = new Tablice();
+            tablice.wczytywanie();
+            tablice.wartosci();
+            Console.WriteLine(tablice.napis);
+            Console.WriteLine("Podaj znak +/* ");
+            char p = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(tablice.mat(p));
+        }
+    }
+}
